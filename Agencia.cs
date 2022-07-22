7cd939249a5c33781a16282af8e2872d4b27435a
Alana Santos de Oliveira <asoliveira@fapex.org.br>
2022-07-22 10:20:04 -0300

    first commit

---
+++ Agencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BANCO
+{
+    class Agencia
+    {
+        public string titular { get; set; }
+        public string conta { get; set; }
+        public int numero_agencia { get; set; }
+        public string nome { get; set; }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BANCO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = -1;
+            Agencia agencia = new Agencia();
+            while(opcao != 0)
+            {
+                int i;
+                Console.WriteLine("***************************** BANCO BR ***************************");
+                Console.WriteLine("\n(1)CADASTRO \n(2)VERIFICAR DADOS");
+                i = int.Parse(Console.ReadLine());
+
+                switch (i)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
